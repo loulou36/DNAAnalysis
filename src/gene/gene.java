6e8d0ae8552b85fb39e50ec919390c2f47d0130d
loulou36 <klogw36@gmail.com>
2023-04-23 13:23:54 +0200

    gene object definition

--- src/gene/gene.java
+++ src/gene/gene.java
@@ -1,9 +1,14 @@
 package gene;
 
 public class gene {
-	String Codename;
-	String name ="";
+	//Accession,Start,Stop,Gene symbol,Strand,NCBI Gene ID,Name
+
+	String accession;
 	int start;
 	int end;
+	String geneSymbol ="";
+	String strand ="";
+	String NCBINGeneID="";
+	String name="";
 
 }
